bc2bc8118b6bbab765dff59d6359e7f7ba9dcdcb
Aybars Hasan Şalvarcı <aybarsalvarci44@gmail.com>
2026-02-22 19:53:19 +0300

    intervention/laravel-image installed, imageHelper configured.

---
+++ app/Http/Services/ImageHelper.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Services;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
+class ImageHelper
+{
+
+    public static function uploadWithoutEncoding(UploadedFile $image,string $path, ?int $width = null, ?string $format = null)
+    {
+        $format = $format ?? $image->extension();
+        $image = Image::read($image);
+
+        if ($width)
+        {
+            $image = $image->scaleDown($width);
+        }
+
+        $image = $image->encodeByExtension($format, quality:80);
+
+        $filename = Str::uuid() . '.' . $format;
+        $fullpath = trim($path, '/') . '/' . $filename;
+
+        Storage::disk('public')->put($fullpath, $image);
+
+        return $fullpath;
+    }
+}
