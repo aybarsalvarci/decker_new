54c491c40714acaf2367e36ecd64e4fc06b3ae26
aybarsalvarci <aybarsalvarci44@gmail.com>
2026-06-17 19:07:23 +0300

    imageService ico ve svg format hatası düzeltildi.

--- app/Http/Services/ImageService.php
+++ app/Http/Services/ImageService.php
@@ -1,25 +1,26 @@
 <?php
 
 namespace App\Http\Services;
+
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Intervention\Image\Laravel\Facades\Image;
+
 class ImageService
 {
 
-    public static function upload(UploadedFile $image,string $path, ?int $width = null, ?string $format = null)
+    public static function upload(UploadedFile $image, string $path, ?int $width = null, ?string $format = null)
     {
         $format = $format ?? $image->extension();
         $image = Image::read($image);
 
-        if ($width)
-        {
+        if ($width) {
             $image = $image->scaleDown($width);
         }
 
         $image = $image->sharpen(12)
-            ->encodeByExtension($format, quality:80);
+            ->encodeByExtension($format, quality: 80);
 
         $filename = Str::uuid() . '.' . $format;
         $fullpath = trim($path, '/') . '/' . $filename;
@@ -30,18 +31,17 @@ class ImageService
 
     }
 
-    public static function uploadWithEncoding(UploadedFile $image,string $path, ?int $width = null, ?string $format = null)
+    public static function uploadWithEncoding(UploadedFile $image, string $path, ?int $width = null, ?string $format = null)
     {
         $format = $format ?? $image->extension();
         $image = Image::read($image);
 
-        if ($width)
-        {
+        if ($width) {
             $image = $image->scaleDown($width);
         }
 
         $image = $image->sharpen(12)
-            ->encodeByExtension($format, quality:80);
+            ->encodeByExtension($format, quality: 80);
 
         $filename = Str::uuid() . '.' . $format;
         $fullpath = trim($path, '/') . '/' . $filename;
@@ -52,23 +52,28 @@ class ImageService
 
     }
 
-    public static function uploadWithoutEncoding(UploadedFile $image,string $path, ?int $width = null)
+    public static function uploadWithoutEncoding(UploadedFile $image, string $path, ?int $width = null)
     {
-        $format = $image->getClientOriginalExtension();
-        $image = Image::read($image);
-
-        if($width)
-        {
-            $image = $image->scaleDown($width);
-        }
-
-        $image = $image->sharpen(12)
-            ->encodeByExtension($format, quality:80);
-
+        $format = strtolower($image->getClientOriginalExtension());
         $fileName = Str::uuid() . '.' . $format;
         $fullpath = trim($path, '/') . '/' . $fileName;
 
-        Storage::disk('public')->put($fullpath, $image);
-        return $path . "/" . $fileName;
+        if (in_array($format, ['ico', 'svg'])) {
+            Storage::disk('public')->putFileAs(trim($path, '/'), $image, $fileName);
+            return trim($path, '/') . "/" . $fileName;
+        }
+
+        $img = Image::read($image);
+
+        if ($width) {
+            $img = $img->scaleDown($width);
+        }
+
+        $encodedImage = $img->sharpen(12)
+            ->encodeByExtension($format, quality: 80);
+
+        Storage::disk('public')->put($fullpath, $encodedImage);
+
+        return trim($path, '/') . "/" . $fileName;
     }
 }
